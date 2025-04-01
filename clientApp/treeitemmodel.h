c7745cc9e6f40819fb0e2ff5191f309269f41560
andruwa_01 <cheprasov.work@gmail.com>
2025-04-01 09:27:10 +0300

    update treeitemmodel.h (refactroing)

--- clientApp/treeitemmodel.h
+++ clientApp/treeitemmodel.h
@@ -52,7 +52,6 @@ public:
     QVariant data(const QModelIndex &index, int role) const override;
     bool setData(const QModelIndex &index, const QVariant &value, int role) override;
     QVariant headerData(int section, Qt::Orientation orientation, int role) const override;
-//    bool setHeaderData(int section, Qt::Orientation orientation, const QVariant &value, int role) override;
     bool insertRows(int row, int count, const QModelIndex &parent) override;
     bool removeRows(int row, int count, const QModelIndex &parent) override;
     Qt::ItemFlags flags(const QModelIndex &index) const override;
